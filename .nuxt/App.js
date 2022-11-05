6807b1a21d4c1d976acf0cb984ad02b8d4238995
raquel_santos1965 <raquel.dossantos1965@gmail.com>
2022-11-05 16:16:38 -0300

    fazendo deploy no hosting do firebase

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -4,11 +4,8 @@ import { decode, parsePath, withoutBase, withoutTrailingSlash, normalizeURL } fr
 import { getMatchedComponentsInstances, getChildrenComponentInstancesUsingFetch, promisify, globalHandleError, urlJoin, sanitizeComponent } from './utils'
 import NuxtError from './components/nuxt-error.vue'
 import NuxtLoading from './components/nuxt-loading.vue'
-import NuxtBuildIndicator from './components/nuxt-build-indicator'
 
 import '../src/assets/scss/global.scss'
-
-import '../node_modules/vuetify/dist/vuetify.css'
 
 import _77263413 from '../src/layouts/clean.vue'
 import _6f6c098b from '../src/layouts/default.vue'
@@ -53,7 +50,7 @@ export default {
       }
     }, [
       loadingEl,
-      h(NuxtBuildIndicator),
+
       transitionEl
     ])
   },
@@ -104,6 +101,10 @@ export default {
 
     isFetching () {
       return this.nbFetching > 0
+    },
+
+    isPreview () {
+      return Boolean(this.$options.previewData)
     },
   },
 
@@ -189,10 +190,6 @@ export default {
     },
 
     setLayout (layout) {
-      if(layout && typeof layout !== 'string') {
-        throw new Error('[nuxt] Avoid using non-string value as layout property.')
-      }
-
       if (!layout || !layouts['_' + layout]) {
         layout = 'default'
       }
