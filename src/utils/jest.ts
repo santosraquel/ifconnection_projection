8157be115878af94ce0d92df50187be729d42fe5
raquel_santos1965 <raquel.dossantos1965@gmail.com>
2022-11-05 13:37:16 -0300

    rodando projeto sem usar o emulador local

--- src/utils/jest.ts
+++ src/utils/jest.ts
@@ -1,3 +1,4 @@
 export const isATest = process.env.JEST_WORKER_ID !== undefined || process.env.NODE_ENV === 'test'
 export const isDev = process.env.NODE_ENV === 'development'
-export const emulate = isATest || isDev
+export const emulate = isATest
+// export const emulate = isATest || isDev
